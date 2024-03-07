96ad5b8191857a1486166db7954312ea15a4d9e3
Daniel Chromik <daniel.chromik@seznam.cz>
2024-03-07 20:40:03 +0100

    Neovim added to windows setup

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,4 +1,5 @@
 winget install --id Microsoft.Powershell --source winget
 winget install --id Starship.Starship
 winget install --id Git.Git
+winget install --id Neovim.Neovim
 winget install --id wez.wezterm
