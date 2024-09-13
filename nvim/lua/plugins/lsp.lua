c29f02ac63ea1a7681d69372123bcaefe3f02426
Daniel Chromik <daniel.chromik@dawiso.com>
2024-09-13 16:40:33 +0200

    Removed eslint config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,17 +4,5 @@ return {
     opts = {
       inlay_hints = { enabled = false },
     },
-    config = function()
-      require("lspconfig").eslint.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        on_new_config = function(config, new_root_dir)
-          config.settings.workspaceFolder = {
-            uri = vim.uri_from_fname(new_root_dir),
-            name = vim.fn.fnamemodify(new_root_dir, ":t"),
-          }
-        end,
-      })
-    end,
   },
 }
