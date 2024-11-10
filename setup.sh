53e953be852172ef522801092a3da8285b5101cf
Daniel Chromik <daniel.chromik@seznam.cz>
2024-11-10 19:19:32 +0100

    Added hyprland and waybar configsa

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 mkdir -p ~/.config
-ln -s `pwd`/nvim ~/.config
-ln -s `pwd`/wezterm ~/.config
-
+ln -s $(pwd)/nvim ~/.config
+ln -s $(pwd)/wezterm ~/.config
+ln -s $(pwd)/hypr ~/.config
+ln -s $(pwd)/waybar ~/.config
