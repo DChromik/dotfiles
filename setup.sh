4d3dbd9f90202cde3b1cb94827967690049551cd
Daniel Chromik <daniel.chromik@dawiso.com>
2025-02-26 09:49:11 +0100

    Switched wezterm to ghostty, removed hypr

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
+curl -sS https://starship.rs/install.sh | sh
+echo 'eval "$(starship init bash)"' >>~/.bashrc
+
 mkdir -p ~/.config
 ln -s $(pwd)/nvim ~/.config
 ln -s $(pwd)/lazygit ~/.config
-ln -s $(pwd)/wezterm ~/.config
-ln -s $(pwd)/hypr ~/.config
-ln -s $(pwd)/waybar ~/.config
-ln -s $(pwd)/waybar ~/.config
-
-curl -sS https://starship.rs/install.sh | sh
-echo 'eval "$(starship init bash)"' >>~/.bashrc
+ln -s $(pwd)/ghostty ~/.config
+ln -s $(pwd)/starship ~/.config
