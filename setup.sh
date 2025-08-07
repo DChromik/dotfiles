f3ae26bbfca8da1985d095f93533836cbf681563
Daniel Chromik <daniel.chromik@seznam.cz>
2025-08-07 21:14:31 +0200

    Added zellij

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ ln -s $(pwd)/nvim ~/.config
 ln -s $(pwd)/lazygit ~/.config
 ln -s $(pwd)/ghostty ~/.config
 ln -s $(pwd)/starship ~/.config
+ln -s $(pwd)/zellij ~/.config
 ln -s $(pwd)/.gitconfig ~/.gitconfig
