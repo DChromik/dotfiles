ec0c6b4d5b88fb4e097fad8888d615995f1d9980
Daniel Chromik <daniel.chromik@proton.me>
2025-11-07 15:04:34 +0100

    Disabled mouse for neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,16 @@
 vim.opt.spell = true
 vim.g.tsc_makeprg = "npx tsc --noEmit"
 vim.g.ai_cmp = false
+vim.g.clipboard = {
+  name = "wl-clipboard-background",
+  copy = {
+    ["+"] = "wl-copy --foreground --type text/plain",
+    ["*"] = "wl-copy --foreground --primary --type text/plain",
+  },
+  paste = {
+    ["+"] = "wl-paste --no-newline --type text/plain",
+    ["*"] = "wl-paste --no-newline --primary --type text/plain",
+  },
+  cache_enabled = 1,
+}
+vim.opt.mouse = ""
--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -3,8 +3,8 @@ return {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {
-      separate_diagnostic_server = true,
       settings = {
+        separate_diagnostic_server = true,
         tsserver_file_preferences = {
           importModuleSpecifierPreference = "non-relative",
         },
